0da875584ad445013aa122e69b0a4c0e66fbb48a
gwanghui97 <lgh970409@gmail.com>
2024-10-10 20:04:57 +0900

    feat: #11 RedisConfig 구현

--- src/main/java/com/pingpong/config/RedisConfig.java
+++ src/main/java/com/pingpong/config/RedisConfig.java
@@ -1,2 +1,66 @@
-package com.pingpong.config;public class RedisConfig {
+package com.pingpong.config;
+
+import com.pingpong.property.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableRedisRepositories
+@Configuration
+public class RedisConfig {
+
+    /**
+     * Redis 설정 정보를 Environment에서 읽어와 RedisProperties 객체를 생성하는 Bean
+     * @param environment Spring의 Environment 객체
+     * @return RedisProperties 설정 정보가 담긴 객체
+     */
+    @Bean
+    public RedisProperties redisProperties(Environment environment) {
+        String host = environment.getProperty("redis.host");
+        int port = Integer.parseInt(environment.getProperty("redis.port", "6379"));
+        String password = environment.getProperty("redis.password");
+        int database = Integer.parseInt(environment.getProperty("redis.database", "0"));
+
+        return RedisProperties.builder()
+                .host(host)
+                .port(port)
+                .password(password)
+                .database(database)
+                .build();
+    }
+
+    /**
+     * RedisConnectionFactory를 생성하는 Bean
+     * @param redisProperties Redis 설정 정보를 담고 있는 객체
+     * @return RedisConnectionFactory 객체
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
+        configuration.setPassword(redisProperties.getPassword());
+        configuration.setDatabase(redisProperties.getDatabase());
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    /**
+     * RedisTemplate을 생성하는 Bean
+     * @param redisProperties Redis 설정 정보를 담고 있는 객체
+     * @return RedisTemplate 객체
+     */
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisProperties redisProperties) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory(redisProperties));
+        return redisTemplate;
+    }
 }
