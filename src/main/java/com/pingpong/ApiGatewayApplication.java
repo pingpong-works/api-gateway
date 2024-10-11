c4a842dcba03a4e5022b6fae52189e25906cabbf
gwanghui97 <lgh970409@gmail.com>
2024-10-11 19:39:03 +0900

    feat: #28 Feign 설정

--- src/main/java/com/pingpong/ApiGatewayApplication.java
+++ src/main/java/com/pingpong/ApiGatewayApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @ConfigurationPropertiesScan
 @EnableDiscoveryClient
 @EnableRedisRepositories
+@EnableFeignClients
 @EnableWebFlux
 @SpringBootApplication
 public class ApiGatewayApplication {
